ee0da9d173d20f631f004609ffd1a07921621c71
serge <serge.moi@laplateforme.io>
2019-10-29 11:33:22 +0100

    Retirer des nombres d'une boucle while.

---
+++ jour02/job02/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+$nombre = 0;
+
+while($nombre <= 1337){
+    
+    if($nombre == 26 || $nombre == 37 || $nombre == 88 || $nombre == 1111 & $nombre == 3233){
+        echo " ";
+    } else {
+        echo $nombre;
+        echo "<br/>";
+    }
+    $nombre++;
+}
+
+
+
+?>
