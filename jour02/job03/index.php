01d40f1018f9ca7cde4da18e7910142f4fc842ed
serge <serge.moi@laplateforme.io>
2019-10-29 11:55:07 +0100

    Utiliser le else if

---
+++ jour02/job03/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$nombre = 0;
+
+while($nombre <= 100){
+    if($nombre <= 20){
+        echo "<i>$nombre</i>";
+        echo "<br/>";
+    } else if ($nombre == 42) {
+        echo "LaPlateforme_";
+        echo "<br/>";
+    } else if ($nombre >= 25 & $nombre <= 50){
+        echo "<u>$nombre</u>";
+        echo "<br/>";
+    } else {
+        echo $nombre;
+        echo "<br/>";
+    }
+    $nombre++;
+}
+
+?>
