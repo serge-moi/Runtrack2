2b54f1e817014982ee6f0340ede93ddf03bccd4d
serge <serge.moi@laplateforme.io>
2019-11-04 10:44:16 +0100

    function affiche true or false

---
+++ jour05/job02/index.php
@@ -0,0 +1,27 @@
+<?php
+
+function bonjour($jour){
+    if($jour == true){
+        echo "Bonjour";
+    } else {
+        echo "Bonsoir";
+    }
+}
+
+$bool = false;
+echo bonjour($bool);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
